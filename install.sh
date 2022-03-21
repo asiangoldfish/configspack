8ecd1274224a20130bd28027865409cc25bb7d7e
Khai <65278376+asiangoldfish@users.noreply.github.com>
2022-03-21 15:54:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/bash
 
+file_exists() (
+    printf "File %s already exists\n" "#1"
+    return 0
+)
+
 if ! command -v curl &> /dev/null; then
     printf "Curl was not found. Install curl before proceeding\n"
 fi
 
 if [ -f ~/.bashrc ]; then
-    printf "Skipping .bashrc\n"
+    file_exists() ".bashrc"
 else
     curl -o ~/.bashrc https://raw.githubusercontent.com/asiangoldfish/configspack/main/bashrc
 fi
 
 if [ -f ~/.nanorc ]; then
-    printf "Skipping .nanorc\n"
+    file_exists() ".nanorc"
 else
     curl -o ~/.nanorc https://raw.githubusercontent.com/asiangoldfish/configspack/main/nanorc
 fi
 
 if [ -f ~/.xinitrc ]; then
-    printf "Skipping .xinitrc\n"
+    file_exists() ".xinitrc"
 else
     curl -o ~/.xinitrc https://raw.githubusercontent.com/asiangoldfish/configspack/main/xinitrc
 fi
